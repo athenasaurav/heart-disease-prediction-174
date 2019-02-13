83b9177103503555ce951cfbbc7b2bdcc8b7c2d3
Song Tian <liust.17@ichat.sp.edu.sg>
2019-02-13 22:43:43 +0800

    Add path and method for dialogflow

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import flask.json as json
 import sys
 import os
 import heart_model as heart
@@ -30,6 +31,48 @@ def heart_predict():
     except Exception as e:
         print('error:', e)
     return str(result)
+
+@app.route('/dialogflow', methods=['post'])
+def dialogflow():
+    result = {'fulfillmentText':'n/a'}
+    try:
+        input = request.get_json()
+        if input['queryResult']['intent']['displayName'] == 'predict':
+            for context in input['queryResult']['outputContexts']:
+                if context['name'].endswith('user-data'):
+                    print('context name: {}'.format(context['name']))
+                    params = context['parameters']
+                    
+                    print(params.get('age'))
+                    print(params.get('sex'))
+                    print(params.get('cp'))
+                    print(params.get('thalach'))
+                    print(params.get('exang'))
+                    print(params.get('oldpeak'))
+                    print(params.get('slope'))
+                    print(params.get('ca'))
+                    print(params.get('thal'))
+
+                    age = int(params.get('age'))
+                    sex = int(params.get('sex'))
+                    cp = int(params.get('cp'))
+                    thalach = int(params.get('thalach'))
+                    exang = int(params.get('exang'))
+                    oldpeak = float(params.get('oldpeak'))
+                    slope = int(params.get('slope'))
+                    ca = int(params.get('ca'))
+                    thal = int(params.get('thal'))
+
+                    output = heart.predict_heart(age, sex, cp, thalach, exang, oldpeak, slope, ca, thal)
+                    print('The prediction is {}'.format(output))
+                    
+                    result = {'fulfillmentText':'Our amazing machine algorithm predicted that you {} a heart disease'.format('have' if output==1 else 'do no have')}
+        else:
+            result = {'fulfillmentText':'Our amazing machine learning algorithm predicted that you may have a heart disease'}
+    except Exception as e:
+        print('error: ', e)
+
+    return json.dumps(result)
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 9099))
